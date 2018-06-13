caeac60a12accce89c69ecf28ff6313c7efd5293
scottclarkeCTS <Collegenn18>
2018-06-13 10:15:17 +0100

    Initial check-in of module musicapp

---
+++ musicapp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace musicapp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        async void Added(object sender, EventArgs e)
+        {
+            DisplayAlert("Added", "The song has been added", "Okay");
+        }
+	}
+}
