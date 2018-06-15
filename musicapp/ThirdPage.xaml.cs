2cc5163d7fdd8290571eae2f9d7919676e8ff38e
scottclarkeCTS <Collegenn18>
2018-06-15 14:51:24 +0100

    Adding in an extra button

--- musicapp/ThirdPage.xaml.cs
+++ musicapp/ThirdPage.xaml.cs
@@ -11,5 +11,9 @@ namespace musicapp
         {
             InitializeComponent();
         }
+        async void ThirdMainPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
     }
 }
